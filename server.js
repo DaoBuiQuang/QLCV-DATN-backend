376ee13f7256e2aaa6f542f1d9d1dcd8e3243c22
QD22082003 <buiquangdao2k3@gmail.com>
2025-03-04 17:39:27 +0700

    update code 4/3 17h39

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 import express from "express";
- 
+import dotenv from "dotenv";
+
+dotenv.config(); // ✅ Đặt lên đầu tiên
+
 import productRouter from "./src/routers/product.js";
 import courseRouter from "./src/routers/courseRouter.js";
+import authRouter from "./src/routers/authRouter.js";
 import { connectDB } from "./src/config/db.js";
 import { syncDatabase } from "./src/model/index.js";
 
- 
 const app = express();
 app.use(express.json());
 app.use("/api", productRouter);
-app.use("/api", courseRouter)
+app.use("/api", courseRouter);
+app.use("/api", authRouter);
+
 connectDB();
-  // Kết nối MySQL
 syncDatabase();
- 
-// Khởi động server
+
 app.listen(3000, () => {
-    console.log("🚀 Server running on http://localhost:5000");
+    console.log("🚀 Server running on http://localhost:3000");
 });
--- src/model/index.js
+++ src/model/index.js
@@ -2,6 +2,7 @@ import { sequelize } from "../config/db.js";
 import { Product } from "./productModel.js";
 import { Course } from "./courseModel.js"; 
 import { Lesson } from "./lessonModel.js";
+import { Auth } from "./authModel.js";
 export const syncDatabase = async () => {
     await sequelize.sync(); 
     console.log("✅ Database synchronized with all models");
