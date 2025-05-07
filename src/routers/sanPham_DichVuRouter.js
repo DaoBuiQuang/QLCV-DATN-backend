82c40afd161ebe886cf7b9dfa355d15c4ed6bc69
QD22082003 <buiquangdao2k3@gmail.com>
2025-05-07 07:51:50 +0700

    git update code

--- src/routers/sanPham_DichVuRouter.js
+++ src/routers/sanPham_DichVuRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/productsandservices/list", getAllSanPhamDichVu);
 router.post("/productsandservices/detail",authenticateUser, getSanPhamDichVuById);
 router.post("/productsandservices/add",authenticateUser, addSanPhamDichVu);
-router.put("/productsandservices/update",authenticateUser, updateSanPhamDichVu);
+router.put("/productsandservices/edit",authenticateUser, updateSanPhamDichVu);
 router.post("/productsandservices/delete",authenticateUser, deleteSanPhamDichVu);
 
 export default router;
